bcf61e9f6b20bab555c15bf2793a2ad831dfbf0a
p-jacobo2012240 <pablojacobo1998@gmail.com>
2021-02-18 21:50:33 -0600

    consume db success

--- mvc-cli/src/mvc/views/PrincipalMenu.java
+++ mvc-cli/src/mvc/views/PrincipalMenu.java
@@ -4,15 +4,30 @@
  * and open the template in the editor.
  */
 package mvc.views;
+import java.sql.ResultSet;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import mvc.connection.ConnectionDB;
 
 /**
  *
  * @author Pablo Jacobo
  */
 public class PrincipalMenu {
+    ConnectionDB cnn = new ConnectionDB();
     
-    public void Menu(){
+    public void Menu() throws ClassNotFoundException{
         System.out.println("holass");
-        String sql = "SELECT * FROM TB_OWNER";
+        try {
+            String sql = "SELECT * FROM TB_OWNER";
+            cnn.open();
+            ResultSet rs = this.cnn.executeQuery(sql);
+            while( rs.next()) {
+                System.out.println(" name "+ rs.getString("NAME_OWNER"));
+            }
+            cnn.close();
+        } catch (Exception ex) {
+            Logger.getLogger(PrincipalMenu.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 }
