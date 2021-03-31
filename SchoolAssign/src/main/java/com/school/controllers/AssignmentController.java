e355f19e8340f2b80137fcc3c3063e7de000e4a7
p-jacobo2012240 <pablojacobo1998@gmail.com>
2021-03-30 22:51:18 -0600

    add doHello method

--- SchoolAssign/src/main/java/com/school/controllers/AssignmentController.java
+++ SchoolAssign/src/main/java/com/school/controllers/AssignmentController.java
@@ -3,6 +3,7 @@ package com.school.controllers;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,6 +15,12 @@ public class AssignmentController {
 
 	private List<Student> studentList = new ArrayList<>();
 	
+	@GetMapping("/assign/hello")
+	public String doHello() {
+		return "Hola dev...";
+	}
+	
+	
 	@PostMapping("/assign/student")
 	public String addStudent(@RequestBody Student student ) {
 		String result = "";
@@ -23,7 +30,5 @@ public class AssignmentController {
 		this.studentList.add(newStudent);
 		return result = "Se agrego correctamente el estudiante " + newStudent.getName();
 	}
-	
-	
 	
 }
